a412cb3def9e7e85ecd4ff7b8513db0816d57912
Bas Kaal <3askaal@gmail.com>
2022-10-31 01:27:12 +0100

    fix removing intersecting lines, progress on detecting shapes

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,6 +1,6 @@
 import Sketch from "react-p5";
 import randomColor from "randomcolor";
-import { forEach, includes, random, sample, remove, slice, sortBy, tail, times } from 'lodash';
+import { forEach, includes, isEqual, random, sample, shuffle, slice, sortBy, tail, times, uniq } from 'lodash';
 import { useEffect, useRef } from "react";
 import type P5 from "p5";
 
@@ -21,20 +21,23 @@ export default function Canvas() {
     // const colors = randomColor({ hue: hue.value, luminosity: luminosity.value, count: 180 })
     const colors = randomColor({ hue: 'blue', luminosity: undefined, count: 180 })
 
-    let points: any[] = []
-
     const amountPoints = Math.round((canvasWidth + canvasHeight) / 8)
 
-    times(amountPoints, (i) => {
+    let points: any[] = times(amountPoints, (i) => {
       const x = random(50, canvasWidth - 50)
       const y = random(50, canvasHeight - 50)
-      points.push({ x, y, i })
+
+      return { x, y, i }
     })
 
     // clear all close points
-    function clearClosePoints(distance: number) {
-      points.forEach((point1) => {
-        remove(points, (point2) => {
+    const clearClosePoints = (distance: number) => {
+      let closePoints: any[] = []
+
+      return points.filter((point1) => {
+        if (closePoints.includes(point1)) return false
+
+        const newClosePoints = points.filter((point2) => {
           const xDistance = Math.abs(point1.x - point2.x)
           const yDistance = Math.abs(point1.y - point2.y)
 
@@ -44,22 +47,28 @@ export default function Canvas() {
 
           return !isCurrentPoint && xIsToClose && yIsToClose
         })
+
+        closePoints = [...closePoints, ...newClosePoints]
+
+        return true
       })
     }
 
-    clearClosePoints(80)
+    points = clearClosePoints(70)
 
-    points.forEach((point) => {
+    points = points.map((point) => {
       // get distance and angle of all points compared to current point
-      let comparedPoints = forEach(points.concat(), (comparingPoint) => {
-
+      let comparedPoints = points.map((comparingPoint) => {
         // diff between current point and comparing point
         const xDiff = point.x - comparingPoint.x
         const yDiff = point.y - comparingPoint.y
 
         // define distance and angle
-        comparingPoint.distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff)
-        comparingPoint.angle = Math.atan2(yDiff, xDiff) * (180 / Math.PI)
+        return {
+          ...comparingPoint,
+          distance: Math.sqrt(xDiff * xDiff + yDiff * yDiff),
+          angle: Math.atan2(yDiff, xDiff) * (180 / Math.PI),
+        }
       })
 
       // sort on distance
@@ -68,19 +77,24 @@ export default function Canvas() {
       // remove first one because it's the same position
       comparedPoints = tail(comparedPoints)
 
-      // slice only closest points
-      point.surroundingPoints = slice(comparedPoints, 0, 6)
+      return {
+        ...point,
+        // slice only closest points
+        surroundingPoints: slice(comparedPoints, 0, random(5, 6))
+      }
     })
 
-    const lines: number[][][] = []
+    let lines: number[][][] = []
 
     // draw lines between points and their surrounding points
-    points.forEach((point, pointIndex) => {
-      remove(point.surroundingPoints, (surroundingPoint: any, surroundingPointIndex) => {
+    points = points.map((point) => {
+      point.surroundingPoints = point.surroundingPoints.reduce((acc: any[], surroundingPoint: any) => {
         if (!includes(surroundingPoint.surroundingPoints, point)) {
-          return true
+          acc.push(surroundingPoint)
         }
-      })
+
+        return acc
+      }, [])
 
       point.surroundingPoints.forEach((surroundingPoint: any, surroundingPointIndex: number) => {
         let next = point.surroundingPoints[surroundingPointIndex+1]
@@ -89,27 +103,77 @@ export default function Canvas() {
           next = point.surroundingPoints[0]
         }
 
-        lines.push([[point.x, point.y], [surroundingPoint.x, surroundingPoint.y]])
+        const newLine = [[point.x, point.y], [surroundingPoint.x, surroundingPoint.y]]
+
+        if (!lines.some((line) => checkLinesIntersect(
+          newLine[0][0], newLine[0][1], newLine[1][0], newLine[1][1],
+          line[0][0], line[0][1], line[1][0], line[1][1],
+        ))) {
+          lines.push(newLine)
+        }
+
       })
+
+      return point
     })
 
-    // clear crossing lines
-    const linesToDraw = lines.filter((line1) => {
-      const [[l1x1, l1y1], [l1x2, l1y2]] = line1
-
-      return !lines.some((line2) => {
-        const [[l2x1, l2y1], [l2x2, l2y2]] = line2
-
-        return checkLinesIntersect(l1x1, l1y1, l1x2, l1y2, l2x1, l2y1, l2x2, l2y2)
-      })
-    })
-
-    linesToDraw.forEach((line) => {
+    lines.forEach((line) => {
       const [[l1x1, l1y1], [l1x2, l1y2]] = line
 
       p5.line(l1x1, l1y1, l1x2, l1y2);
       p5.stroke(sample(colors) as string);
     })
+
+    // const uniquePoints: number[][] = uniq(lines.flat().map((point) => sortBy(point)))
+
+    // console.log(uniquePoints)
+
+    // points.forEach((p: any) => {
+    //   console.log(p)
+
+    //   p.surroundingPoints.map((surroundingPoint: any) => {
+
+    //   })
+    // })
+
+    const getNextSurroundingPointCW = (initP: any, p: any): any => {
+      const shape: any[] = []
+
+      const sp: [number, number] = [p.x, p.y]
+      const point = points.find(({x, y}) => isEqual([x, y], sp))
+
+      const next = point.surroundingPoints.sort((p: any) => p.angle > sp.angle ? 1 : -1)[0]
+      console.log(next)
+
+      const shapeComplete = isEqual(next, initP)
+
+      return shapeComplete ? shape : getNextSurroundingPointCW(initP, next)
+    }
+
+    // define shapes
+    const shapes = points.reduce((acc, point, i) => {
+
+      // console.log(point)
+      const p: [number, number] = [point.x, point.y]
+
+      if (!i) {
+        p5.stroke('white');
+        p5.strokeWeight(10);
+        p5.point(...p);
+        console.log(point);
+
+        // getNextSurroundingPointCW(point, point)
+
+        point.surroundingPoints.map((surroundingPoint: any) => {
+          const sp: [number, number] = [surroundingPoint.x, surroundingPoint.y]
+          const point = points.find(({x, y}) => isEqual([x, y], sp))
+
+          p5.stroke('red');
+          p5.strokeWeight(5);
+          p5.point(...sp);
+        })
+      }
+    }, [])
   }
 
   const checkLinesIntersect = (...points: number[]) => {
